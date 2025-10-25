10121fc98b08c71513f60296a4b70a4563cb8d90
YRadwan <43502481+YusRadwan@users.noreply.github.com>
2025-10-25 17:21:48 +0300

    Day 5 - Higher Order Function

--- Ass-Week10 _ Higher Order Functions/main.js
+++ Ass-Week10 _ Higher Order Functions/main.js
@@ -3,5 +3,22 @@ console.log(`%%% Assignments Week 10 %%%`);
 
 // Task Video
 
+/* 1)
+    let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+    let result = myString.split(',').filter((ele) => {
+        return isNaN(parseInt(ele));
+    }).map((ele) => {
+        return ele[+false] !== '_' ? ele : ' ';
+    }).reduce((aac, curr) => {
+        return (aac + curr)
+    })
+
+    let one = +true;
+
+    console.log(result.slice(one, -one));
+*/
+
+
 
 // Assignments Website
