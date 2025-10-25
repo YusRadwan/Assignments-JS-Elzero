cec1cc4949f949ef1fcc78c4d87242d8330c32e3
YRadwan <43502481+YusRadwan@users.noreply.github.com>
2025-10-25 08:26:59 +0300

    Day 5

--- Ass-Week8/main.js
+++ Ass-Week8/main.js
@@ -61,4 +61,76 @@ console.log(`%%% Assignments Week 8 %%%`);
     sayHello("Sameh"); // "Hello Sameh"
 */
 
+/* 2)
+function calculate(firstNum, secondNum, operation) {
+    if (firstNum === undefined || secondNum === undefined) {
+        document.writeln(`Second Number Not Found <hr>`)
+    } else {
+        if(operation === 'add') {
+            return document.writeln(`Calculator = ${firstNum + secondNum}<hr>`);
+        } else if (operation === 'subtract') {
+            return document.writeln(`Calculator = ${firstNum - secondNum}<hr>`);
+        } else if (operation === 'multiply') {
+            return document.writeln(`Calculator = ${firstNum * secondNum}<hr>`);
+        } else {
+            return document.writeln(`Calculator = ${firstNum + secondNum}<hr>`);
+        }
+    }
+}
+
+// Needed Output
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, 'add'); // 50
+calculate(20, 30, 'subtract'); // -10
+calculate(20, 30, 'multiply'); // 600
+*/
+
+/* 3)
+    function ageInTime(theAge) {
+    if (theAge > 10 && theAge < 100) {
+        console.log(`${theAge * 12} Months`);
+        console.log(`${theAge * 12 * 4} Weeks`);
+        console.log(`${theAge * 12 * 4 * 7} Days`);
+        console.log(`${theAge * 12 * 4 * 7 * 24} Hours`);
+        console.log(`${theAge * 12 * 4 * 7 * 24 * 60} Mintes`);
+        console.log(`${theAge * 12 * 4 * 7 * 24 * 60 * 60} Seconds`);
+    } else {
+        console.log(`Age Out Of Range`);
+    }
+    }
+
+    // Needed Output
+    ageInTime(110); // Age Out Of Range
+    console.log(`------------`);
+    ageInTime(38); // Months Example => 456 Months
+    console.log(`------------`);
+    ageInTime(26); // Months Example => 312 Months
+*/
+
+/* 5)
+    function createSelectBox(startYear, endYear) {
+        let year = [];
+        document.writeln(`<select>`)
+            for (let i = startYear; i <= endYear; i++) {
+                document.writeln(`<option value="${i}">${i}</option>`);
+            }
+        document.writeln(`</select>`)
+    }
+    createSelectBox(2000, 2021);
+*/
+
+/* 6)
+    function multiply(...arg) {
+        for (let i = 0; i < arg.length; i++) {
+            if (typeof arg[i] === 'string') {continue;} 
+            return document.writeln(`Equal => ${Math.floor(arg[i]) * arg[i + 1]}<hr>`);
+        }
+    }
+
+    multiply(10, 20); // 200
+    multiply("A", 10, 30); // 300
+    multiply(100.5, 10, "B"); // 1000
+*/
+
 
