ac7c84118076555c6e96b177fb5a288e7dfd02c0
YRadwan <43502481+YusRadwan@users.noreply.github.com>
2025-10-25 18:48:45 +0300

    Day 5 - Higher Order Function

--- Ass-Week10 _ Higher Order Functions/main.js
+++ Ass-Week10 _ Higher Order Functions/main.js
@@ -19,6 +19,65 @@ console.log(`%%% Assignments Week 10 %%%`);
     console.log(result.slice(one, -one));
 */
 
-
-
 // Assignments Website
+
+/* 1)
+    let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+
+    let result = mix.map((ele) => {
+        return isNaN(parseInt(ele)) ? ele : '';
+    }).reduce((acc, cur) => {
+        return acc + cur
+    });
+
+    console.log(result)
+
+    // Elzero
+*/
+
+/* 2)
+    let myString = "EElllzzzzzzzeroo";
+
+    let result = myString.split('').filter((ele, index, array) => {
+        return ele !== array[index - 1];
+    }).join('');
+
+    console.log(result)
+    // Elzero
+*/
+
+/* 3)
+    let myArray = ["E", "l", "z", ["e", "r"], "o"];
+
+    let result = myArray.reduce((acc, curr, index, array) => {
+        return acc.concat(curr)
+    }, [])
+
+
+    console.log(result)
+    // Elzero
+*/
+
+/* 4)
+    let numsAndStrings = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
+
+    let result = numsAndStrings
+        .map(ele => typeof ele === 'number' ? -ele : '')
+        .filter(ele => ele)
+    console.log(result);
+
+    // [-1, -10, 10, 20, -5, -3]
+*/
+
+/* 5)
+    let nums = [2, 12, 11, 5, 10, 1, 99];
+
+    let result = nums.reduce((acc, curr) => {
+        return (curr % 2 === 0) ? acc * curr : acc + curr;
+    }, 1)
+
+    console.log(result)
+
+    // 500
+*/
+
