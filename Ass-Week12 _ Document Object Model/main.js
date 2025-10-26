5dddbfb71da35d86d8e6fd0fb8053cd64089c6fc
YRadwan <43502481+YusRadwan@users.noreply.github.com>
2025-10-27 00:26:11 +0300

    Day 6 DOM

--- Ass-Week12 _ Document Object Model/main.js
+++ Ass-Week12 _ Document Object Model/main.js
@@ -19,7 +19,7 @@ console.log(document.getElementsByClassName('element'));
 console.log(document.body.getElementsByClassName('element'));
 */
 
-/*
+/* 2)
 let divElement = document.getElementsByTagName('div');
 
 console.log(divElement)
@@ -30,6 +30,68 @@ for (let i = 0; i < divElement.length; i++) {
 }
 */
 
+/* 3)
+let divPrice = document.getElementsByClassName('result');
+let inputPrice = document.getElementsByName('dollar');
+
+inputPrice[0].addEventListener('keyup', (e) => {
+    let egValue = e.target.value, 
+        dollarValue = ((e.target.value) * 15.6).toFixed(2);
+    divPrice[0].innerHTML = 
+        `{${egValue}} USD Dollar = {${dollarValue}} Egyptian Pound`;
+});
+*/
+
+/* 4)
+let divOne = document.getElementsByClassName('one');
+let divTwo = document.getElementsByClassName('two');
+divOne.item('div.one').innerHTML = 'One';
+divOne.item('div.one').setAttribute('title', 'one');
+divTwo.item('div.two').innerHTML = 'Two 2';
+divTwo.item('div.one').setAttribute('title', 'two');
+*/
+
+/* 5)
+let imgElement = document.querySelectorAll('img');
+
+for (let i = 0; i < imgElement.length; i++) {
+    if(imgElement[i].hasAttribute('alt')) {
+        imgElement[i].setAttribute('alt', 'Old');
+    } else {
+        imgElement[i].setAttribute('alt', 'Elzero New');
+    }
+}
+*/
+
+/* 6)
+let formEle = document.getElementsByTagName('form');
+let numberOfEle = document.getElementsByName('elements');
+let numbertext = document.getElementsByName('texts');
+let selectEle = document.getElementsByName('type');
+let buttonSubmit = document.getElementsByName('create');
+let divEle = document.querySelector('.results');
+let divEleChild = document.querySelector('.box');
 
 
+    buttonSubmit[0].onclick = (e) => {
+        e.preventDefault();
+
+        if (divEle.hasChildNodes()) {
+            divEle.innerHTML = '';
+        }
+
+        for (let i = 1; i <= numberOfEle[0].value; i++) {
+            // if (selectEle)
+            let divCreate = document.createElement('div');
+            // let divCreate = document.createElement('section');
+
+            divCreate.className = "box";
+            divCreate.title = "Element";
+            divCreate.id = `id-${i}`;
+            divCreate.textContent = numbertext[0].value;
+
+            divEle.appendChild(divCreate);
+        }
+    }
+*/
 
