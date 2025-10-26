d9a03ccf953e38cc57ef3895cf63543e408949e6
YRadwan <43502481+YusRadwan@users.noreply.github.com>
2025-10-26 14:26:03 +0300

    Day 6

---
+++ Ass-Week12 _ Document Object Model/main.js
@@ -0,0 +1,35 @@
+console.log(`%%% ASS-Week 12 %%%`);
+
+// Assignments Weebsite
+
+/* 1)
+console.log(document.body.children);
+console.log(document.body.childNodes);
+console.log(document.querySelector('div'));
+console.log(document.querySelectorAll('div'));
+console.log(document.getElementsByName('js'));
+console.log(document.getElementById('elzero'));
+console.log(document.querySelector('#elzero'));
+console.log(document.querySelector('.element'));
+console.log(document.querySelectorAll('#elzero'));
+console.log(document.querySelector('div.element'));
+console.log(document.querySelectorAll('.element'));
+console.log(document.querySelectorAll('div.element'));
+console.log(document.getElementsByClassName('element'));
+console.log(document.body.getElementsByClassName('element'));
+*/
+
+/*
+let divElement = document.getElementsByTagName('div');
+
+console.log(divElement)
+
+for (let i = 0; i < divElement.length; i++) {
+    divElement[i].childNodes[1].setAttribute('src', 'https://elzero.org/wp-content/themes/elzero/imgs/logo.png')
+    divElement[i].childNodes[1].setAttribute('alt', 'Elzero Logo')
+}
+*/
+
+
+
+
